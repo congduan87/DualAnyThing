e0340efd207af0a0da3dd7f27f74721c69ead426
duanvc <duanvc@outlook.com>
2021-03-14 15:35:32 +0700

    aaaa

---
+++ DualAnyThing/Dual.MVC.App/Controllers/CropImageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dual.MVC.App.Controllers
+{
+    public class CropImageController : Controller
+    {
+        // GET: CropImage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DualAnyThing/Dual.MVC.App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dual.MVC.App.MvcApplication" Language="C#" %>
